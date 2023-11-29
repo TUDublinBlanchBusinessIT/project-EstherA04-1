6bfbb98ac391a8216f80892064d67083e993e2e2
unknown <estherakinlade04@gmail.com>
2023-11-29 10:38:19 +0000

    added a comment

--- project-1.php
+++ project-1.php
@@ -6,7 +6,7 @@ $password = "";
 $dbname = "your_database_name";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+//added a comment here
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
